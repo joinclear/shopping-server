691b99a35ff9768d368cd492b45c173d07805316
jxx <704977788@qq.com>
2020-04-20 09:56:12 +0800

    初始化多模块项目

---
+++ shopping-bi/src/test/java/com/shopping/bi/ShoppingBiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.bi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingBiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ shopping-common/src/test/java/com/shopping/common/ShoppingCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
